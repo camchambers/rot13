2fb83301eed2e3f613eb65c90280eea41bf56ad0
Cam Chambers <cam_chambers@outlook.com>
2021-09-03 13:43:21 -0400

    Now reads input from stdin

--- rot13.py
+++ rot13.py
@@ -1,16 +1,19 @@
+#!/usr/bin/python3
+
+import sys
 # Define a translation table to perform the mapping between characters
 rot13trans = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-                       'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+                           'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
 
-# A function to perform the translation
+
 def rot13(text):
     """ Translate the text """
     return text.translate(rot13trans)
 
 
 def main():
-    txt = "The quick brown fox jumps over the lazy dog"
-    print(rot13(txt))
+    for line in sys.stdin:
+        sys.stdout.write(rot13(line))
 
 
 if __name__ == "__main__":
